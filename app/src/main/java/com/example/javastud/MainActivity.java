6a49a2c2ffa83db12ee495ba35537733b0a43602
beyzamercan97 <beyzamercan@yonsei.ac.kr>
2021-10-03 14:32:22 +0300

    no message

---
+++ app/src/main/java/com/example/javastud/MainActivity.java
@@ -0,0 +1,54 @@
+package com.example.javastud;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import com.example.javastud.databinding.ActivityMainBinding;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+
+public class MainActivity extends AppCompatActivity {
+    private ActivityMainBinding binding;
+    private final Random random = new Random();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        binding.button.setOnClickListener(v -> updateLotto());
+    }
+
+    private void updateLotto() {
+        List<Integer> arr = makeNewLotto();
+        binding.textNum1.setText(arr.get(0).toString());
+        binding.textNum2.setText(arr.get(1).toString());
+        binding.textNum3.setText(arr.get(2).toString());
+        binding.textNum4.setText(arr.get(3).toString());
+        binding.textNum5.setText(arr.get(4).toString());
+        binding.textNum6.setText(arr.get(5).toString());
+    }
+
+    private List<Integer> makeNewLotto () {
+        ArrayList<Integer> array = new ArrayList<>();
+
+        while (array.size() != 6) {
+            int nextValue = random.nextInt(45) + 1;
+            Log.i("realone", String.format("Random value is %d", nextValue));
+            if (!array.contains(nextValue)) {
+                array.add(nextValue);
+            } else {
+                Log.i("realone", "중복이라 다시 뽑니다");
+            }
+
+        }
+        Collections.sort(array);
+        return array;
+
+    }
+}
